60a142c22246e36f610bd5ee9f310d4b110ea571
Kougar7 <100093574+GuilhermeOliveiraSouz@users.noreply.github.com>
2023-11-10 22:30:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const cellsWithEvent = document.querySelectorAll(".tem-evento");
 
-  cellsWithEvent.forEach(cell => {
-    cell.addEventListener("click", function() {
-      // logica javascript aqui
-      alert("Mostrar eventos para o dia " + cell.innerText);
-    });
-  });
-});
